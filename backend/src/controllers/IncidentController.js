2d6d05864e9173109f307e50c409bc8d8b44bd4e
Gustavo Carmo <gustavo.silva.carmo@hotmail.com>
2020-03-29 12:14:54 -0300

    Fix bug on search incidents

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -6,9 +6,9 @@ module.exports = {
     const { page = 1 } = request.query;
 
     const [ count ] = await connection('incidents').count();
-
+    
     const incidents = await connection('incidents')
-      .join('ongs', 'ong_id', '=', 'incidents.ong_id')
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
       .limit(5)
       .offset((page - 1) * 5)
       .select(
